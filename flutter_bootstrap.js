3563f2c3875ec90c61638e54f396f00bce2fc8b3
Kyaw Thet Aung <developer@mdgmyanmar.com>
2026-08-25 10:24:10 +0630

    update portfolio version 2.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3072119727" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3479542469" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
